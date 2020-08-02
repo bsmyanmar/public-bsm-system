6d1164bbabc3dcd56809df91ae067a84637d5ba1
Cing Sian Dal <csiandal@gmail.com>
2020-08-02 18:36:29 +0630

    Create CONSTANTS.py

---
+++ CONSTANTS.py
@@ -0,0 +1,45 @@
+
+# Book counts of New Testament + Old Testament
+NT_OT_SIZE = 66
+
+# Book counts of New Testament + Psalms + Proverbs
+NT_2P_SIZE = 29
+
+# Book counts of New Testament Only
+NT_SIZE = 27
+
+# Abbreviation code of New Testament + Old Testament
+NT_OT_ARRAY = ["GEN", "EXO", "LEV", "NUM", "DEU", "JOS", "JDG", "RUT", "1SA", "2SA", "1KI", "2KI", "1CH", "2CH", "EZR",
+               "NEH", "EST", "JOB", "PSA", "PRO", "ECC", "SNG", "ISA", "JER", "LAM", "EZK", "DAN", "HOS", "JOL", "AMO",
+               "OBA", "JON",
+               "MIC", "NAM", "HAB", "ZEP", "HAG", "ZEC", "MAL", "MAT", "MRK", "LUK", "JHN", "ACT", "ROM", "1CO", "2CO",
+               "GAL", "EPH", "PHP", "COL", "1TH", "2TH", "1TI", "2TI", "TIT", "PHM", "HEB", "JAS", "1PE", "2PE", "1JN",
+               "2JN", "3JN", "JUD", "REV"]
+
+# Abbreviation code of New Testament + Psalms + Proverbs
+NT_2P_ARRAY = ["MAT", "MRK", "LUK", "JHN", "ACT", "ROM", "1CO", "2CO", "GAL", "EPH", "PHP", "COL", "1TH", "2TH",
+               "1TI", "2TI", "TIT", "PHM", "HEB", "JAS", "1PE", "2PE", "1JN", "2JN", "3JN", "JUD", "REV", "PSA", "PRO"]
+
+# Abbreviation code of New Testament Only
+NT_ARRAY = ["MAT", "MRK", "LUK", "JHN", "ACT", "ROM", "1CO", "2CO", "GAL", "EPH", "PHP", "COL", "1TH",
+            "2TH", "1TI", "2TI", "TIT", "PHM", "HEB", "JAS", "1PE", "2PE", "1JN", "2JN", "3JN", "JUD", "REV"]
+
+# Valid markers as of USFM Version 3.0
+VALID_MARKERS = ['id', 'usfm', 'ide', 'h', 'h1', 'h2', 'h3', 'toc1', 'toc2', 'toc3', 'toca1', 'toca2', 'toca3', 'rem',
+                 'sts', 'restore', 'imt', 'imt1', 'imt2', 'imt3', 'imt4', 'imte', 'imte1', 'imte2', 'is', 'is1', 'is2',
+                 'iot', 'io', 'io1', 'io2', 'io3', 'io4', 'ior', 'ip', 'im', 'ipi', 'imi', 'ili', 'ili1', 'ili2', 'ipq',
+                 'imq', 'ipr', 'ib', 'iq', 'iq1', 'iq2', 'iq3', 'iex', 'iqt', 'ie', 'c', 'ca', 'cp', 'cl', 'cd', 'v',
+                 'va', 'vp', 'p', 'm', 'po', 'pr', 'cls', 'pmo', 'pm', 'pmc', 'pmr', 'pi', 'pi1', 'pi2', 'pi3', 'pc',
+                 'mi', 'nb', 'q', 'q1', 'q2', 'q3', 'q4', 'qc', 'qr', 'qs', 'qa', 'qac', 'qm', 'qm1', 'qm2', 'qm3',
+                 'qd', 'b', 'mt', 'mt1', 'mt2', 'mt3', 'mt4', 'mte', 'mte1', 'mte2', 'ms', 'ms1', 'ms2', 'ms3', 'mr',
+                 's', 's1', 's2', 's3', 's4', 'sr', 'r', 'sp', 'd', 'sd', 'sd1', 'sd2', 'sd3', 'sd4', 'tr', 'th1',
+                 'th2', 'th3', 'th4', 'th5', 'tc1', 'tc2', 'tc3', 'tc4', 'tc5', 'thc1', 'thc2', 'thc3', 'thc4', 'thc5',
+                 'tcc1', 'tcc2', 'tcc3', 'tcc4', 'tcc5', 'thr1', 'thr2', 'thr3', 'thr4', 'thr5', 'tcr1', 'tcr2', 'tcr3',
+                 'tcr4', 'tcr5', 'lh', 'li', 'li1', 'li2', 'li3', 'li4', 'lf', 'lim', 'lim1', 'lim2', 'lim3', 'lim4',
+                 'litl', 'lik', 'liv', 'liv1', 'liv2', 'liv3', 'liv4', 'liv5', 'f', 'fe', 'fr', 'ft', 'fk', 'fq', 'fqa',
+                 'fl', 'fw', 'fp', 'fv', 'fdc', 'fm', 'x', 'xo', 'xop', 'xt', 'xta', 'xk', 'xq', 'xot', 'xnt', 'xdc',
+                 'rq', 'qt', 'nd', 'tl', 'dc', 'bk', 'sig', 'pn', 'png', 'addpn', 'wj', 'k', 'sls', 'ord', 'add', 'lit',
+                 'no', 'it', 'bd', 'bdit', 'em', 'sc', 'sup', 'pb', 'fig', 'jmp', 'pro', 'rb', 'w', 'wh', 'wg', 'wa',
+                 'ndx', 'periph', 'p1', 'p2', 'k1', 'k2', 'xtSee', 'xtSeeAlso', 'ph', 'ph1', 'ph2', 'ph3', 'phi', 'tr1',
+                 'tr2', 'ps', 'psi', 'fs', 'wr', 'pub', 'toc', 'pref', 'intro', 'conc', 'glo', 'idx', 'maps', 'cov',
+                 'spine', 'pubinfo', 'zpa', 'zpa', 'zpa', 'zpa']
